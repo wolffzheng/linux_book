c7dc64b3cad66d37e3ae31a8f07458c7a4fb91fc
wolffzheng <wolff_zheng@hotmail.com>
2015-04-08 10:40:53 +1000

    add books

--- upload.sh
+++ upload.sh
@@ -7,12 +7,13 @@ git add . --all
 git commit -m "add books"
 git checkout master
 git merge tmp
-(sleep 1
-echo "wolffzheng"
-sleep 1
-echo "Wolff8341") | git push origin master
-#git push origin master << EOF
-#wolffzheng Wolff8341
-#EOF
+#(sleep 1
+#echo "wolffzheng"
+#sleep 1
+#echo "Wolff8341") | git push origin master
+git push origin master << HHH
+wolffzheng 
+Wolff8341
+HHH
 git branch -d tmp
 
